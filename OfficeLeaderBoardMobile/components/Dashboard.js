c6d559b74df0b1b8ac5870b752f4706d3bb2f5a5
Taylor Burrington <taylor.burrington@gmail.com>
2019-08-07 21:15:46 -0700

    iconAdditions

--- OfficeLeaderBoardMobile/components/Dashboard.js
+++ OfficeLeaderBoardMobile/components/Dashboard.js
@@ -1,108 +1,118 @@
 import { ScrollView, Text, Image } from "react-native";
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import styles from "./Styles";
 import { Button, Avatar, Input, ListItem, Card } from "react-native-elements";
-import Icon from "react-native-vector-icons/FontAwesome";
-import {LinearGradient} from 'expo-linear-gradient';
-import Pong from '../assets/images/pingpong.jpg'
-import SmashBros from '../assets/images/smashbros.jpg'
-import RageCage from '../assets/images/ragecage.jpg'
-
+// import Icon from "react-native-vector-icons/FontAwesome";
+import { LinearGradient } from "expo-linear-gradient";
+import Pong from "../assets/images/pingpong.jpg";
+import SmashBros from "../assets/images/smashbros.jpg";
+import RageCage from "../assets/images/ragecage.jpg";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const RaisedButton = props => <Button raised {...props} />;
 
-const images = [
-  RageCage, SmashBros, Pong
-]
+const images = [RageCage, SmashBros, Pong];
 
 const games = [
   {
-    name: 'Ping Pong',
+    name: "Ping Pong",
     users: {
-      username: 'Soroush',
+      username: "Soroush",
       rank: "Rank: 1st"
-    },
- 
+    }
   },
   {
-    name: 'SmashBros.',
+    name: "SmashBros.",
     users: {
-      username: 'Vance',
-      rank: 'Rank: 2nd'
-    },
-
+      username: "Vance",
+      rank: "Rank: 2nd"
+    }
   },
   {
-    name: 'Rage Cage',
+    name: "Rage Cage",
     users: {
-      username: 'Neftali',
-      rank: 'Rank: 3rd'
-    },
-
+      username: "Neftali",
+      rank: "Rank: 3rd"
+    }
   }
-]
+];
 export default class Dashboard extends Component {
-    static navigationOptions = ({ navigation }) => {
-      return {
-        headerTitle: (
-          <Avatar
-            rounded
-            source={{
-              uri:
-                "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"
-            }}
-          />
-        )
-      };
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerTitle: (
+        <Avatar
+          rounded
+          source={{
+            uri:
+              "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"
+          }}
+        />
+      )
     };
-    render() {
-      const { navigation } = this.props;
-      const itemId = navigation.getParam("itemId", "NO-ID");
-      const otherParam = navigation.getParam("otherParam", "some default value");
-      return (
-        <LinearGradient colors={['#03738c', '#024059']}>
+  };
+  render() {
+    const { navigation } = this.props;
+    const itemId = navigation.getParam("itemId", "NO-ID");
+    const otherParam = navigation.getParam("otherParam", "some default value");
+    return (
+      <LinearGradient colors={["#03738c", "#024059"]}>
         <ScrollView>
-        
           {/* <Text>DetailsScreen</Text>
           <Text>itemId: {JSON.stringify(itemId)}</Text>
           <Text>otherParm: {JSON.stringify(otherParam)}</Text> */}
-          <Text>GameCreatorHere...</Text>
+
           <Input
-            placeholder="Game Input"
-            leftIcon={<Icon name="user" size={24} color="#ffffff" />}
+            placeholder="     ...game input"
+            placeholderTextColor="#777"
+          
+            leftIcon={<Icon name="plus-circle" size={24} color="#ffffff" />}
           />
           {/* <Divider style ={{backgroundColor : "#777"}}/>; */}
-          <Text>GameContainerGoesHere...</Text>
-            {games.map((u, i) => {
-              return (
-                // <Image key={i} source={require(images[i])}/>
-                <Card key={i} title={u.name} image={images[i]} titleStyle={{color: '#fff'}} containerStyle={{padding: 0, borderRadius: 10, overflow: 'hidden', backgroundColor: 'transparent'}}>
+
+          {games.map((u, i) => {
+            return (
+              // <Image key={i} source={require(images[i])}/>
+              <Card
+                key={i}
+                title={u.name}
+                image={images[i]}
+                imageStyle={{
+                  margin: 10,
+                  borderTopLeftRadius: 10,
+                  borderBottomLeftRadius: 10,
+                  overflow: "hidden"
+                }}
+                titleStyle={{ color: "#fff", textDecorationLine: "underline" }}
+                containerStyle={{
+                  padding: 0,
+                  borderRadius: 10,
+                  overflow: "hidden",
+                  backgroundColor: "transparent"
+                }}
+              >
                 {games.map((u, i) => {
                   return (
-                    
                     <ListItem
-                      containerStyle={{backgroundColor: 'transparent'}}
-                      
+                      containerStyle={{ backgroundColor: "transparent" }}
                       key={i}
                       color="#ffffff"
                       title={u.users.username}
                       subtitle={u.users.rank}
-                      titleStyle={{color: '#fff'}}
-                      subtitleStyle={{color: '#fff'}}
+                      titleStyle={{ color: "#fff" }}
+                      subtitleStyle={{ color: "#fff" }}
                       chevron
-                      />
-                  )
+                    />
+                  );
                 })}
               </Card>
-              )
-            })}
-                 <Button
-                icon={<Icon name='code' color='#ffffff' />}
+            );
+          })}
+          {/* <Button
+                icon={<Icon color='#ffffff' />}
                 backgroundColor='#03A9F4'
                 buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-                title='VIEW NOW' />
-        
-    
+                title='VIEW NOW' /> */}
+
           {/* <RaisedButton
             title="Go to Dashboard...again"
             onPress={() =>
@@ -124,9 +134,8 @@ export default class Dashboard extends Component {
             onPress={() => this.props.navigation.goBack()}
           /> */}
           {/* <TabNavigation  />s */}
-         
         </ScrollView>
-        </LinearGradient>
-      );
-    }
+      </LinearGradient>
+    );
   }
+}
