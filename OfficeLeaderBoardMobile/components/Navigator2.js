b376e8243068d4fa4ce3f99d88e0504eddfdbdbb
Taylor Burrington <taylor.burrington@gmail.com>
2019-08-07 17:01:09 -0700

    "starterCode"

---
+++ OfficeLeaderBoardMobile/components/Navigator2.js
@@ -0,0 +1,35 @@
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+// import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+
+import {HomeScreen, GameScreen, Dashboard} from './Screens.js';
+
+
+const TabNavigator = createBottomTabNavigator (
+    {
+        First: {screen: Dashboard},
+        Second: {screen: HomeScreen},
+        Third: {screen: GameScreen}
+    }, 
+    {
+        tabBarOptions: {
+            activeTintColor: 'orange',
+
+            inactiveTintColor: 'blue',
+
+            activeBackgroundColor: 'green',
+
+            inactiveBackgroundColor: 'yellow',
+
+            style: {borderWidth: 2, borderColor: 'purple'},
+
+            labelStyle: {fontWeight: 'bold'},
+
+            tabStyle: {paddingBottom: 16},
+
+            
+        }
+    }
+)
+
+const AppTabNavigator = createAppContainer(TabNavigator);
+export default AppTabNavigator;
