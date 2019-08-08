562034457eca2afff6458105be61dd4215154bad
Neftali Dominguez <n.l.dominguez23@gmail.com>
2019-08-08 11:12:51 -0700

    imported reducres into index.js

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -10,9 +10,13 @@
  */
 
 import { combineReducers } from 'redux';
+import gameReducer from './gameReducer';
+import userReducer from './userReducer';
 
 const reducers = combineReducers({
-    // if we had other reducers, they would go here
-    game: gameReducer,
-    user: userReducer
-  });
+  // if we had other reducers, they would go here
+  game: gameReducer,
+  user: userReducer,
+});
+
+export default reducers;
